8c9d47c64f1c844901a2862fb2cce67edf39155e
Pablo Acebedo <pablofelipe@me.com>
2023-01-23 09:49:19 -0500

    Technologyes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,22 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+        Unleash the Power of <br />
+        Blockchain Technology 🚀
+
+        </SectionTitle>
+        <SectionText>
+        We are a leading provider of innovative blockchain-based solutions for businesses and organizations. Our team of experts is dedicated to helping you leverage the power of blockchain technology to streamline your operations, improve efficiency, and drive growth.
+        </SectionText>
+        <Button onClick={() => window.location = 'https://www.pabloacebedo.com/'}>Contact</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import {SiArchicad } from 'react-icons/si';
+import { FaAnchor, FaSnapchatGhost } from "react-icons/fa";
+import { SiAirplayaudio } from "react-icons/si";
+
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+  <SectionDivider divider />
+  <SectionTitle>Technologies</SectionTitle>
+  <SectionText>
+  Token creation is the process of creating and issuing digital tokens on a blockchain network. These tokens can be used for a variety of purposes, such as representing a digital asset, a utility within a decentralized application, or as a form of currency.
+  </SectionText>
+  <List>
+    <ListItem>
+      <picture>
+        <FaAnchor size="3rem" />
+      </picture>
+      <ListContainer>
+        <ListTitle>Framework</ListTitle>
+        <ListParagraph>
+          Anchor <br />
+          For quickly building secure Solana programs.
+        </ListParagraph>
+      </ListContainer>
+    </ListItem>
+    <ListItem>
+      <picture>
+        <FaSnapchatGhost size="3rem" />
+      </picture>
+      <ListContainer>
+        <ListTitle>Wallet</ListTitle>
+        <ListParagraph>
+          Phantom <br />
+          Safe and easy to store, send, receive, collect, and swap tokens on the Solana blockchain.
+        </ListParagraph>
+      </ListContainer>
+    </ListItem>
+    <ListItem>
+      <picture>
+        <SiArchicad size="3rem" />
+      </picture>
+      <ListContainer>
+        <ListTitle>AMM</ListTitle>
+        <ListParagraph>
+        Liquidity Bootstrapping <br />
+        Curve - LBC
+        </ListParagraph>
+      </ListContainer>
+    </ListItem>
+   
+  </List>
+  <SectionDivider colorAlt />
+</Section>
 );
 
 export default Technologies;
